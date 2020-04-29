3b1596e3546fb0a07c913ac29e63a4e904c5ac9a
ielson <danmascandrade@gmail.com>
2020-04-29 18:46:08 -0300

    Primeiro commit, fazendo o groupy dos dados

---
+++ automate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import datetime
+
+print("Iniciando o programa")
+data_atual = datetime.date.today()
+if data_atual.month == 2 and data_atual.day == 29:
+    data_atual = data_atual.replace(day=28)
+data_limite = data_atual.replace(year=2018)
+print("Data de vencimento das Tales: {}".format(data_limite))
+
+data_planilha = "20_04_07"
+print("Data da planilha: {}".format(data_planilha))
+base_instalada_path = "{}/Base Instalada.xlsx".format(data_planilha)
+consumiveis_path = "{}/Consumíveis MR.xlsx".format(data_planilha)
+
+base_instalada = pd.read_excel(base_instalada_path, sheet_name='Planilha3')
+print("Tamanho da base instalada: {}".format(base_instalada.shape[0]))
+consumiveis = pd.read_excel(consumiveis_path, sheet_name='Planilha1')
+
+base_instalada_tales = base_instalada[base_instalada['TALES'] == 'Y']
+print("Dessas {} tem tales".format(base_instalada_tales.shape[0]))
+consumiveis_tales = consumiveis[(consumiveis['Material'] == 10018247) | (consumiveis['Material'] == 7391886)]
+
+consumiveis_tales_novas = consumiveis_tales[consumiveis_tales['Dt.Consumo'].dt.date > data_limite]
+print("Das Tales substituidas {} estão no prazo".format(consumiveis_tales_novas.shape[0]))
+
+consumiveis_tales_novas_destination = consumiveis_tales_novas[(consumiveis_tales_novas['Destinação Mat.'] == 'CB - CONSUMED BILLABLE') |
+                                                  (consumiveis_tales_novas['Destinação Mat.'] == 'C - CONSUMED')]
+print("Das Tales novas {} foram consumidas".format(consumiveis_tales_novas_destination.shape[0]))
+
+#teste = base_instalada_tales[consumiveis_tales_novas_destination['Nº série'] in base_instalada_tales['Nº de série']]
+base_tales_vencida = base_instalada_tales[~base_instalada_tales['Nº de série'].isin(consumiveis_tales_novas_destination['Nº série'])]
+print("Número de Tales Vencidas: {}".format(base_tales_vencida.shape[0]))
+
+numero_por_estado = base_tales_vencida.groupby(['Rg', 'Nome de lista'])['TALES'].count()
+print(numero_por_estado)
